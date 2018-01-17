dcf33470fa55bd04d4cce81d01fdbcb189208f3c
Stephen Harrison <Stephen.Harrison@AnalysisUK.com>
2018-01-17 09:06:38 +0000

    Corrected fan filter build, added neopixel switch and holder, commented out 120mm fans for a bit as not using those parts at this time.

--- Arduino/Enclosure/ReallyDustyBox/Build.bat
+++ Arduino/Enclosure/ReallyDustyBox/Build.bat
@@ -5,6 +5,9 @@ del *.stl
 
 @echo Building Really Dusty Box STLs
 
+@echo Neopixel And Button
+"C:\Program Files\OpenSCAD\openscad.com" -o NeopixelAndSwitchHolder.stl NeopixelAndSwitchHolder.scad
+
 @echo Spacers
 "C:\Program Files\OpenSCAD\openscad.com" -o Spacer.stl Spacer.scad
 
@@ -19,27 +22,27 @@ del *.stl
 "C:\Program Files\OpenSCAD\openscad.com" -o FilterFrame-3x3-Retainer.stl -D "isInsideFrame=true;depth=2;holesDown=3;holesAcross=3" FilterFrame.scad
 
 @echo Mini Fan Filter
-"C:\Program Files\OpenSCAD\openscad.com" -o FanFilter-120-Mini.stl -D "width=120;height=120;totalDepth=60" FanFilter.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o FanFilter-140-Mini.stl -D "width=140;height=140;totalDepth=60" FanFilter.scad
+#"C:\Program Files\OpenSCAD\openscad.com" -o FanFilter-120-Mini.stl -D "width=120;height=120;totalDepth=95" FanFilter.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o FanFilter-140-Mini.stl -D "width=140;height=140;totalDepth=95" FanFilter.scad
 
 @echo Normal Fan Filter
-"C:\Program Files\OpenSCAD\openscad.com" -o FanFilter-120-Std.stl -D "width=120;height=120;totalDepth=150" FanFilter.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o FanFilter-140-Std.stl -D "width=140;height=140;totalDepth=150" FanFilter.scad
+#"C:\Program Files\OpenSCAD\openscad.com" -o FanFilter-120-Std.stl -D "width=120;height=120;totalDepth=160" FanFilter.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o FanFilter-140-Std.stl -D "width=140;height=140;totalDepth=160" FanFilter.scad
 
 @echo Tall Fan Filter
-"C:\Program Files\OpenSCAD\openscad.com" -o FanFilter-120-Tall.stl -D "width=120;height=120;totalDepth=260" FanFilter.scad
+#"C:\Program Files\OpenSCAD\openscad.com" -o FanFilter-120-Tall.stl -D "width=120;height=120;totalDepth=260" FanFilter.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o FanFilter-140-Tall.stl -D "width=140;height=140;totalDepth=260" FanFilter.scad
 
 @echo Fan Difuser - Partial
-"C:\Program Files\OpenSCAD\openscad.com" -o FanDifuser-120-OpenTop.stl -D "width=120;height=120;totalDepth=60;openTop=true" FanFilter.scad
-"C:\Program Files\OpenSCAD\openscad.com" -o FanDifuser-140-OpenTop.stl -D "width=140;height=140;totalDepth=60;openTop=true" FanFilter.scad
+#"C:\Program Files\OpenSCAD\openscad.com" -o FanDifuser-120-OpenTop.stl -D "width=120;height=120;totalDepth=60;openTop=true" FanDifuser.scad
+"C:\Program Files\OpenSCAD\openscad.com" -o FanDifuser-140-OpenTop.stl -D "width=140;height=140;totalDepth=60;openTop=true" FanDifuser.scad
 
 @echo Fan Difuser - Full
-"C:\Program Files\OpenSCAD\openscad.com" -o FanDifuser-120-Std.stl -D "width=120;height=120;totalDepth=100;openTop=false" FanDifuser.scad
+#"C:\Program Files\OpenSCAD\openscad.com" -o FanDifuser-120-Std.stl -D "width=120;height=120;totalDepth=100;openTop=false" FanDifuser.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o FanDifuser-140-Std.stl -D "width=140;height=140;totalDepth=100;openTop=false" FanDifuser.scad
 
 @echo Fan Adaptor - 100mm
-"C:\Program Files\OpenSCAD\openscad.com" -o FanAdaptor-120-2-100.stl -D "width=120;height=120;ductSize=100;coneHeight=20" FanAdaptor.scad
+#"C:\Program Files\OpenSCAD\openscad.com" -o FanAdaptor-120-2-100.stl -D "width=120;height=120;ductSize=100;coneHeight=20" FanAdaptor.scad
 "C:\Program Files\OpenSCAD\openscad.com" -o FanAdaptor-140-2-100.stl -D "width=140;height=140;ductSize=100;coneHeight=40" FanAdaptor.scad
 
 @echo Electronics Enclosure
